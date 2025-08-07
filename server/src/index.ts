0ed219073e47380753a1f1abd20fa8a0452353e5
Maxim <max.semchishin@gmail.com>
2025-08-07 18:29:56 +0500

    misspell fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,5 +40,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.POT || 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
